959b62ce3ac314c33bd5a963cf2d68eb4be2fbcb
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2021-07-31 19:02:12 +0530

    It's Pixys time
    - Support for Gapps.
    - Support for pixel offline charging animation.
    
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Change-Id: Ib79885d4ce0c6f4fddc44dbf40543bf10ee90205

--- pixys_whyred.mk
+++ pixys_whyred.mk
@@ -9,8 +9,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common superior stuff
-$(call inherit-product, vendor/superior/config/common.mk)
+# Inherit some common Pixys stuff
+$(call inherit-product, vendor/pixys/config/common_full_phone.mk)
 
 # Inherit from whyred device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -21,11 +21,12 @@ $(call inherit-product, vendor/MiuiCamera/config.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := whyred
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := superior_whyred
+PRODUCT_NAME := pixys_whyred
 PRODUCT_MODEL := Redmi Note 5
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 TARGET_VENDOR_PRODUCT_NAME := whyred
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_PIXEL_CHARGER := true
