7da1a8631b546e0a9687b1503e9efdbc65741378
S I P U N <sipunkumar85@gmail.com>
2024-03-01 03:29:06 +0000

    whyred: PixysOS Bringup
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/superior_whyred.mk
+    $(LOCAL_DIR)/pixys_whyred.mk
 
 COMMON_LUNCH_CHOICES := \
-    superior_whyred-user \
-    superior_whyred-userdebug \
-    superior_whyred-eng
+    pixys_whyred-user \
+    pixys_whyred-userdebug \
+    pixys_whyred-eng
